aca0c9614a532d7bff19d12b76cd948a983a08c4
Necnion8 <necnionch@gmail.com>
2022-09-23 09:44:04 +0900

    Fix: Citizensと連携したらNPCをリフレッシュさせる

--- src/main/java/com/gmail/necnionch/myplugin/cestagegenerator/bukkit/hooks/CitizensBridge.java
+++ src/main/java/com/gmail/necnionch/myplugin/cestagegenerator/bukkit/hooks/CitizensBridge.java
@@ -1,15 +1,20 @@
 package com.gmail.necnionch.myplugin.cestagegenerator.bukkit.hooks;
 
+import com.gmail.necnionch.myplugin.cestagegenerator.bukkit.StageGeneratorPlugin;
+import com.gmail.necnionch.myplugin.cestagegenerator.bukkit.game.Game;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPCRegistry;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+
 public class CitizensBridge {
+    private final StageGeneratorPlugin plugin;
     private boolean available;
 
-    public CitizensBridge(Plugin owner) {
-//        this.plugin = owner;
+    public CitizensBridge(StageGeneratorPlugin owner) {
+        plugin = owner;
     }
 
     public boolean hook(@Nullable Plugin plugin) {
@@ -22,6 +27,13 @@ public class CitizensBridge {
             }
             available = true;
         }
+
+        if (available)
+            try {
+                onEnable();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
         return available;
     }
 
@@ -35,6 +47,15 @@ public class CitizensBridge {
 
     public boolean isAvailable() {
         return available;
+    }
+
+    public void onEnable() {
+        Collection<Game> games = plugin.getGameManager().games().values();
+        for (Game game : games) {
+            if (game.getWorld() != null && game.getCurrentStageConfig() != null) {
+                game.getCurrentStageConfig().restoreNPCs(getNPCRegistry(), game.getWorld());
+            }
+        }
     }
 
 }
